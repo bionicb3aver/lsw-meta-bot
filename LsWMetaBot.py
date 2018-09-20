6a57cabcbff3c3c791b3d310ef703cf6690ed2e7
DeatPlayer <43141240+DeatPlayer@users.noreply.github.com>
2018-09-20 21:56:37 +0200

    Added console log when command is executed

--- LsWMetaBot.py
+++ LsWMetaBot.py
@@ -6,8 +6,12 @@ dispatcher = updater.dispatcher
 def meta(bot, update):
     bot.send_message(chat_id=update.message.chat_id, 
         text="Warning. System discovered a meta question.\nIs there a concrete question on the topic ? (Y/n):\n(Was ist eine Metafrage: http://metafrage.de/)")
+    localtime = time.asctime( time.localtime(time.time()) )
+    print(localtime, " - Meta Befehl wurde ausgeführt!")
 def help(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="LsW Metafragen-Bot. https://github.com/DeatPlayer/lsw-meta-bot")
+    localtime = time.asctime( time.localtime(time.time()) )
+    print(localtime, " - Help Befehl wurde ausgeführt!")
 
 meta_handler = CommandHandler('meta', meta)
 dispatcher.add_handler(meta_handler)
