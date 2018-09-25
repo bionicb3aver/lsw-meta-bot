dc0d546c25c839b11d3c3a24f35a73561e0e252c
DeatPlayer <43141240+DeatPlayer@users.noreply.github.com>
2018-09-25 11:38:04 +0200

    Update LsWMetaBot.py
    - RegEx updated
    - Texts and RegEx stored in variables

--- LsWMetaBot.py
+++ LsWMetaBot.py
@@ -6,24 +6,26 @@ import re
 updater = Updater(token='TELEGRAM-BOT-TOKEN')
 dispatcher = updater.dispatcher
 
+regex_pattern = "/(würde|ist|kennt|kann|darf|hat).+(jemand|jemande[m|n]|dich|euch|zufällig|mir).+(mit.+[a-zA-Z](.+auskennt\?|.+aus\?|.+aus[,| ]+der.+kann\?|helfen\?)|fragen\?|jemand helfen\?|(mir|uns) bei [a-zA-Z ]+ helfen\?)/gmi"
+meta_text = "Warning. System discovered a meta question.\nIs there a concrete question on the topic ? (Y/n):\n(Was ist eine Metafrage: http://metafrage.de/)"
+help_text = "LsW Metafragen-Bot. https://github.com/DeatPlayer/lsw-meta-bot"
+
 def meta(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, 
-        text="Warning. System discovered a meta question.\nIs there a concrete question on the topic ? (Y/n):\n(Was ist eine Metafrage: http://metafrage.de/)")
+    bot.send_message(chat_id=update.message.chat_id, text=meta_text)
     localtime = time.asctime( time.localtime(time.time()) )
     sys.stdout.write(localtime + " [INFO] Command: /meta - Chat-ID: " + update.message.chat_id + " => " + update.message.text)
 
 def help(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="LsW Metafragen-Bot. https://github.com/DeatPlayer/lsw-meta-bot")
+    bot.send_message(chat_id=update.message.chat_id, text=help_text)
     localtime = time.asctime( time.localtime(time.time()) )
     sys.stdout.write(localtime + " [INFO] Command: /help - Chat-ID: " + update.message.chat_id + " => " + update.message.text)
     
 def metadetector(bot, update):
     message = update.message.text
-    pattern = re.compile("(ist|kennt|kann|darf|hat).+(jemand|dich|euch|zufällig|mir).+(mit.+[a-zA-Z](.+auskennt\?|.+aus?|helfen\?|\?)|fragen\?|jemand helfen\?|helfen\?)", re.IGNORECASE)
+    pattern = re.compile(regex_pattern, re.IGNORECASE)
     detected = pattern.search(message)
     if detected:
-        bot.send_message(chat_id=update.message.chat_id,
-                         text = "Warning. System discovered a meta question.\nIs there a concrete question on the topic ? (Y/n):\n(Was ist eine Metafrage: http://metafrage.de/)")
+        bot.send_message(chat_id=update.message.chat_id, text=meta_text)
     localtime = time.asctime( time.localtime(time.time()) )
     sys.stdout.write(localtime + " [INFO] Command: autodetect - Chat-ID: " + update.message.chat_id + " => " + update.message.text)
 
