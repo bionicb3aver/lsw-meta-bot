633a744a5c5bb662cf546e725f968285a38c888c
Tim <43141240+DeatPlayer@users.noreply.github.com>
2019-02-28 21:31:23 +0100

    Cooldown für autodetect
    The autodetect function now has a cooldown

--- LsWMetaBot.py
+++ LsWMetaBot.py
@@ -7,27 +7,36 @@ updater = Updater(token='TELEGRAM-BOT-TOKEN')
 dispatcher = updater.dispatcher
 
 regex_pattern = "(würde|ist|kennt|kann|darf|hat).+(jemand|jemande[m|n]|dich|euch|zufällig|mir).+(mit.+[a-zA-Z](.+auskennt\?|.+aus\?|.+aus[,| ]+der.+kann\?|helfen\?)|fragen\?|jemand helfen\?|(mir|uns) bei [a-zA-Z ]+ helfen\?)"
-meta_text = "Warning. System discovered a meta question.\nIs there a concrete question on the topic ? (Y/n):\n(Was ist eine Metafrage: http://metafrage.de/)"
+meta_text = "Warnung! System hat eine Metafrage entdeckt. Gibt es eine konkrete Frage zum Thema? (J/n):\n(Was ist eine Metafrage: http://metafrage.de/)"
 help_text = "LsW Metafragen-Bot. https://github.com/DeatPlayer/lsw-meta-bot"
 
-def meta(bot, update):
+#auto-reply cooldown in secounds
+min_call_freq = 30
+used = {}
+
+
+def meta(bot: Bot, update: Update):
     bot.send_message(chat_id=update.message.chat_id, text=meta_text)
     localtime = time.asctime( time.localtime(time.time()) )
-    sys.stdout.write(localtime + " [INFO] Command: /meta - Chat-ID: " + update.message.chat_id + " => " + update.message.text)
+    print(localtime + ' >> [INFO] Command: /meta')
 
 def help(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=help_text)
     localtime = time.asctime( time.localtime(time.time()) )
-    sys.stdout.write(localtime + " [INFO] Command: /help - Chat-ID: " + update.message.chat_id + " => " + update.message.text)
-    
+    print(localtime + ' >> [INFO] Command: /help')
+
 def metadetector(bot, update):
     message = update.message.text
     pattern = re.compile(regex_pattern, re.IGNORECASE)
     detected = pattern.search(message)
     if detected:
-        bot.send_message(chat_id=update.message.chat_id, text=meta_text, reply_to_message_id=update.message.message_id)
-    localtime = time.asctime( time.localtime(time.time()) )
-    sys.stdout.write(localtime + " [INFO] Command: autodetect - Chat-ID: " + update.message.chat_id + " => " + update.message.text)
+        localtime = time.asctime( time.localtime(time.time()) )
+        if ( "autodetected" not in used or time.time() - used["autodetected"] > min_call_freq):
+          used["autodetected"] = time.time()
+          bot.send_message(chat_id=update.message.chat_id, text=meta_text, reply_to_message_id=update.message.message_id)
+          print(localtime + ' >> [INFO] Command: autodetect')
+        else:
+          print(localtime + ' >> [INFO] Cooldown active..')
 
 autoresponse_handler = MessageHandler(Filters.text, metadetector)
 dispatcher.add_handler(autoresponse_handler)
