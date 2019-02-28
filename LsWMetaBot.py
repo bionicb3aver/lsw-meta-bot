5f112bdb83e966832723aeb5f158aca63f6d0cba
Adrian Sausenthaler <github@sausenthaler.de>
2019-03-01 00:17:53 +0100

    Removed unused imports, typos, added imports

--- LsWMetaBot.py
+++ LsWMetaBot.py
@@ -1,6 +1,6 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler
+from telegram import Update, Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import time
-import sys
 import re
 
 updater = Updater(token='TELEGRAM-BOT-TOKEN')
@@ -10,17 +10,16 @@ regex_pattern = "(würde|ist|kennt|kann|darf|hat).+(jemand|jemande[m|n]|dich|euc
 meta_text = "Warnung! System hat eine Metafrage entdeckt. Gibt es eine konkrete Frage zum Thema? (J/n):\n(Was ist eine Metafrage: http://metafrage.de/)"
 help_text = "LsW Metafragen-Bot. https://github.com/DeatPlayer/lsw-meta-bot"
 
-#auto-reply cooldown in secounds
+#auto-reply cooldown in seconds
 min_call_freq = 30
 used = {}
 
-
-def meta(bot: Bot, update: Update):
+def fnmeta(bot: Bot, update: Update):
     bot.send_message(chat_id=update.message.chat_id, text=meta_text)
     localtime = time.asctime( time.localtime(time.time()) )
     print(localtime + ' >> [INFO] Command: /meta')
 
-def help(bot, update):
+def fnhelp(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=help_text)
     localtime = time.asctime( time.localtime(time.time()) )
     print(localtime + ' >> [INFO] Command: /help')
@@ -31,7 +30,7 @@ def metadetector(bot, update):
     detected = pattern.search(message)
     if detected:
         localtime = time.asctime( time.localtime(time.time()) )
-        if ( "autodetected" not in used or time.time() - used["autodetected"] > min_call_freq):
+        if "autodetected" not in used or time.time() - used["autodetected"] > min_call_freq:
           used["autodetected"] = time.time()
           bot.send_message(chat_id=update.message.chat_id, text=meta_text, reply_to_message_id=update.message.message_id)
           print(localtime + ' >> [INFO] Command: autodetect')
@@ -41,10 +40,10 @@ def metadetector(bot, update):
 autoresponse_handler = MessageHandler(Filters.text, metadetector)
 dispatcher.add_handler(autoresponse_handler)
 
-meta_handler = CommandHandler('meta', meta)
+meta_handler = CommandHandler('meta', fnmeta)
 dispatcher.add_handler(meta_handler)
 
-help_handler = CommandHandler('help', help)
+help_handler = CommandHandler('help', fnhelp)
 dispatcher.add_handler(help_handler)
 
 updater.start_polling()
